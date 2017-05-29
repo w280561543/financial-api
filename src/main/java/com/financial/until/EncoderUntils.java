4d9f98cbf73368d29af6a94c945ad6e82742706f
Maing <280561543@qq.com>
2017-05-30 01:03:56 +0800

    Create: 创建密码加密工具.

---
+++ src/main/java/com/financial/until/EncoderUntils.java
@@ -0,0 +1,41 @@
+package com.financial.until;
+
+import java.lang.CharSequence;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * 密码编码工具类
+ * @author Maing<280561543@qq.com>
+ * @version 0.0.1
+ */
+public class EncoderUntils {
+	/**
+	 * 定义一个BCryptPasswordEncoder的静态私有变量
+	 */
+	private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(8);
+
+	/**
+	 * 定义一个私有构造方法
+	 */
+	private EncoderUntils() {}
+
+	/**
+	 * 加密密码
+	 * @param rawPassword
+	 * @return String 返回加密后的密码
+	 */
+	public static String encode(CharSequence rawPassword) {
+		return encoder.encode(rawPassword);
+	}
+	
+	/**
+	 * 判断密码是否正确
+	 * @param rawPassword - the raw password to encode and match.
+	 * @param encodedPassword - the encoded password from storage to compare with.
+	 * @return boolean
+	 */
+	public static boolean matches(CharSequence rawPassword, String encodedPassword) {
+		return encoder.matches(rawPassword, encodedPassword);
+	}
+}
