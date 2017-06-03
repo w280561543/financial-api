dce41d8e03b751eceff5c82aa8ba49f9a1dbea18
Maing <280561543@qq.com>
2017-06-03 13:30:35 +0800

    Update: Change filename.

--- src/main/java/com/financial/config/SecurityConfiguration.java
+++ src/main/java/com/financial/config/SecurityConfiguration.java
@@ -2,19 +2,26 @@ package com.financial.config;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
+import com.financial.service.UserService;
+import com.financial.until.EncoderUntils;
+
+@Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
@@ -26,10 +33,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			//.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
 	}
 
+	@Autowired
+	private UserService userService;
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(this.userService).passwordEncoder(EncoderUntils.getEncoder());
+	}
+	/*
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
 			.withUser("user").password("password").roles("USER");
 	}
+	*/
 	
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
