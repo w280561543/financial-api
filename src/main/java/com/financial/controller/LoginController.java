89a17442ddd2faff754c57fb5e66ff4d9e4bb61c
Maing <280561543@qq.com>
2017-06-06 15:35:04 +0800

    Update: 已经清楚问题在Security.

--- src/main/java/com/financial/controller/LoginController.java
+++ src/main/java/com/financial/controller/LoginController.java
@@ -7,31 +7,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-import com.financial.model.User;
 /**
  * 登陆控制器类
  * @author Maing<280561543@qq.com>
  */
 @RestController
 public class LoginController {
-    @Autowired
-    private com.financial.service.FinUserDetailsService finUserDetailsService;
-    
-    //@RequestMapping(value = "/login", method=RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    @RequestMapping(value = "/login", method=RequestMethod.POST)
-	//public Map<String, Object> login(@RequestBody Map<String, Object> userInfo) {
-    //public @ResponseBody Map<String, Object> login(@RequestParam(value = "email") String email, @RequestParam(value = "password") String password) {
-    //public Map<String, Object> login(@RequestBody(required = false) User user) {
-    public Map<String, Object> login(@RequestBody(required = false) User user) {
+    @RequestMapping(value = "/login", method=RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public @ResponseBody Map<String, Object> login(@RequestBody Map<String, Object> userInfo) {
 		Map<String, Object> model = new HashMap<String, Object>();
 		model.put("code", 1);
         model.put("status", 200);
 		model.put("message", "Login!");
-        //model.put("data", userInfo);
+        model.put("data", userInfo);
 		return model;
 	}
 
